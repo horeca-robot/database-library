96d148048b252b2976650f31b25a753debab97d0
Gilsing,Guylian Gilsing G <g.gilsing@student.fontys.nl>
2021-10-01 09:41:29 +0200

    Working product entity with constructor, getters and setters

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+# Example Main.java file for testing purposes
 
+```java
+package edu.fontys.horecarobot.databaselibrary;
+
+import edu.fontys.horecarobot.databaselibrary.models.Product;
+import org.hibernate.Session;
+
+public class Main {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        // Insert a new product into the databsae
+        Product product = new Product("Test", "image-b64", 62.32, 62.32, "Super cool description :D", true);
+        session.save(product);
+
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
+    }
+}
+```
--- src/main/java/edu/fontys/horecarobot/databaselibrary/models/Product.java
+++ src/main/java/edu/fontys/horecarobot/databaselibrary/models/Product.java
@@ -3,7 +3,6 @@ package edu.fontys.horecarobot.databaselibrary.models;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.util.UUID;
 
 @Entity
 @Table(name = "products")
@@ -36,7 +35,69 @@ public class Product {
     @Column(name = "contains_alcohol")
     private boolean containsAlcohol;
 
-    public String getId() {
-        return id;
+    public Product(String name, String image, double price, double discountPrice, String description, boolean containsAlcohol) {
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.discountPrice = discountPrice;
+        this.description = description;
+        this.containsAlcohol = containsAlcohol;
     }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscountPrice() {
+        return this.discountPrice;
+    }
+
+    public void setDiscountPrice(double discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getContainsAlcohol() {
+        return this.containsAlcohol;
+    }
+
+    public void setContainsAlcohol(boolean containsAlcohol) {
+        this.containsAlcohol = containsAlcohol;
+    }
+    
 }
