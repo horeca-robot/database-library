0e41e164979ff7de13390bc18a8963c821853aaa
pieterdebot <pieter.de.bot@gmail.com>
2021-10-07 16:26:35 +0200

    Added repositories

--- src/main/java/edu/fontys/horecarobot/databaselibrary/repositories/CategoryRepository.java
+++ src/main/java/edu/fontys/horecarobot/databaselibrary/repositories/CategoryRepository.java
@@ -1,11 +1,11 @@
 package edu.fontys.horecarobot.databaselibrary.repositories;
 
-import edu.fontys.horecarobot.databaselibrary.models.Product;
+import edu.fontys.horecarobot.databaselibrary.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
 }
--- src/main/java/edu/fontys/horecarobot/databaselibrary/repositories/RestaurantTableRepository.java
+++ src/main/java/edu/fontys/horecarobot/databaselibrary/repositories/RestaurantTableRepository.java
@@ -1,11 +1,11 @@
 package edu.fontys.horecarobot.databaselibrary.repositories;
 
-import edu.fontys.horecarobot.databaselibrary.models.Product;
+import edu.fontys.horecarobot.databaselibrary.models.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, UUID> {
 }
