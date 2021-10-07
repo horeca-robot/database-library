88d6b13575ca55caf30fbc35f75996cc544e0cc8
pieterdebot <pieter.de.bot@gmail.com>
2021-10-07 16:16:57 +0200

    Fixed compilation error

--- src/main/java/edu/fontys/horecarobot/databaselibrary/Main.java
+++ src/main/java/edu/fontys/horecarobot/databaselibrary/Main.java
@@ -6,12 +6,14 @@ import edu.fontys.horecarobot.databaselibrary.models.RestaurantTable;
 
 import org.hibernate.Session;
 
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
 
-        RestaurantTable table = new RestaurantTable("id", 1, 1, 2);
+        RestaurantTable table = new RestaurantTable(UUID.randomUUID(), 1, 1, 2);
         session.save(table);
 
         Order order = new Order();
