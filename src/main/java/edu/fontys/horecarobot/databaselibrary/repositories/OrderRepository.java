c7b05726b8489d4d124f66faf6a6005a7ca4d3d1
pieterdebot <pieter.de.bot@gmail.com>
2021-10-12 12:00:45 +0200

    Added documentation

--- src/main/java/edu/fontys/horecarobot/databaselibrary/repositories/OrderRepository.java
+++ src/main/java/edu/fontys/horecarobot/databaselibrary/repositories/OrderRepository.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * These repositories can be used like you would use any other {@link JpaRepository}.
+ * See <a href="https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html">https://docs.spring.io</a>
+ */
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
 }
