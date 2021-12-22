37e8e62e3c9b03b335384f839eb321d5d105a51b
Quint <quint.legius@iqfx.nl>
2021-12-22 13:17:08 +0100

    fixed sort query

--- src/main/java/edu/fontys/horecarobot/databaselibrary/repositories/RestaurantOrderRepository.java
+++ src/main/java/edu/fontys/horecarobot/databaselibrary/repositories/RestaurantOrderRepository.java
@@ -14,5 +14,5 @@ import java.util.UUID;
  */
 @Repository
 public interface RestaurantOrderRepository extends JpaRepository<RestaurantOrder, UUID> {
-    Page<RestaurantOrder> findByOrderByCreatedAtDescOrderDoneDesc(Pageable pageable);
+    Page<RestaurantOrder> findAllByOrderByOrderDoneAscCreatedAtAsc(Pageable pageable);
 }
